15a8729c51d389ec65af44694aa26f77e4061261
mandrav <mandrav@2a5c6006-c6dd-42ca-98ab-0921f2732cef>
2007-06-21 09:32:12 +0000

    * updated manual page

--- web/manual.php
+++ web/manual.php
@@ -4,15 +4,23 @@ require_once('prepend.php');
 <title>Manual</title>
 <h1>Manual</h1>
 
+<p>
 There's an on-going effort to write a manual for using Code::Blocks. This is a
 community-driven effort and contributions/criticism/suggestions are welcomed.
+</p>
 
+<p>
 The initial documentation had started as an internal project of
 <a href="www.hightec-rt.com">HighTec EDV-Systeme GmbH</a> who is now
 making it yet another contribution to the community.
+</p>
 
+<p>
 The documentation is provided in English and German, in PDF format.
 More formats will follow soon.
+</p>
 
-<a href="docs/manual_en.pdf">English manual</a>
-<a href="docs/manual_de.pdf">German manual</a>
+<ul>
+<li><a href="docs/manual_en.pdf">English manual</a></li>
+<li><a href="docs/manual_de.pdf">German manual</a></li>
+</ul>
